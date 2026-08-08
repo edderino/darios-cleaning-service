95a0f227a1db97b4051a5c706ef9f1fc9563ffe5
Adrian Morosin <theredincorporated@gmail.com>
2026-08-08 17:20:38 +1000

    Add Vercel Web Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import ScrollToHash from './components/ScrollToHash'
@@ -19,6 +20,7 @@ export default function App() {
         </Routes>
       </main>
       <Footer />
+      <Analytics />
     </>
   )
 }
